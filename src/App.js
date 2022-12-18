c7492574d530e697ed18785be9c0c0a2c5845426
노수혁 <rohsuhyeok@nosuhyeog-ui-MacBookAir.local>
2022-12-18 14:05:04 +0900

    두 번째

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>테스트</h1>
+        <h2>두 번째</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
